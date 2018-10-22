333ffaa3b3192cb62457961032db4d41bf025891
Marion Schleifer <marion.schleifer@gmail.com>
2018-10-22 21:08:58 +0200

    add number guessing game

---
+++ numberGuessingGame/script.js
@@ -0,0 +1,18 @@
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+let secretNumber = generateRandomNumber();
+console.log(secretNumber);
+
+function checkNumber() {
+  let value = document.getElementById('input').value;
+  if (value == secretNumber) {
+    alert('You won!');
+  } else if (value < secretNumber) {
+    document.getElementById('result').innerHTML = value + ' is too low.';
+  } else {
+    document.getElementById('result').innerHTML = value + ' is too high.';
+  }
+  return false;
+}
